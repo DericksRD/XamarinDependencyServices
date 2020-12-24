5a830786381ad2eea88a2e79c2e884263f82bbe4
Derick <derick290801@gmail.com>
2020-12-24 15:29:28 -0400

    Custom Entry completed

---
+++ DependencyServices/DependencyServices.Android/Renderers/MyEntryRenderer.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using DependencyServices.Controls;
+using DependencyServices.Droid.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(MyEntryRenderer))]
+namespace DependencyServices.Droid.Renderers
+{
+    //Android
+    class MyEntryRenderer : EntryRenderer
+    {
+        public MyEntryRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control != null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Rgb(60, 245, 161));
+                Control.SetTextColor(Android.Graphics.Color.White);
+            }
+        }
+    }
+}
